5e14dd9427b54d3ecd33ddbf32416ffbdace4e91
Eliyaan <nopana1007@gmail.com>
2024-11-06 22:26:07 +0100

    explanation of updates and starting update_cycle function
    explanation of updates and wip fn update_cycle

--- main.v
+++ main.v
@@ -1,4 +1,41 @@
-// Explain update system & tick delay
+// A tick is a unit of time. For each tick, a complete update cycle/process will be effected.
+// Update process: 
+// 1. change which states lists are the actual ones (/!\ when creating/destroying an element, the program must update the actual and the old state lists)
+// 2. for each element in the element lists (nots, diodes, wires) do steps 3 and 4
+// 3. look at the previous state(s) of the input(s) in the old states lists
+// 4. update it's state (in the actual state list and in the id stored in the chunks) accordingly
+// It is updated like a graph to avoid running into update order issues.
+fn (mut app App) update_cycle() {
+	//1. done
+	app.actual_state = (app.actual_state+1)%2
+	//2. done
+	for not in app.nots {
+		//3. 
+		//4. wip
+	}
+	for diode in app.diodes {
+		//3. wip
+		//4. wip
+	}
+	for wire in app.wires {
+		//3. wip
+		//4. wip
+	} 
+}
+
+const rid_bitmap = 0x07FF_FFFF_FFFF_FFFF // 0000_0000_011111... bit map to get the real id with &
+
+fn (mut app App) get_elem_state_by_id(id u64) {
+	// the state in the id may be an old state so it needs to get the state from the state lists
+	if id & 0xC000_0000_0000_0000 == 0b00  // not
+ 		
+	} else if id & 0xC000_0000_0000_0000 == 0b01 { // diode 
+	}
+	} else if id & 0xC000_0000_0000_0000 == 0b10 { // On
+	}
+	} else if id & 0xC000_0000_0000_0000 == 0b11 { // wire
+	}
+}
 
 // Explain ids
 
@@ -20,7 +57,7 @@ struct Nots {
 	y u32
 }
 
-// A gate that transmit the input signal to the output element (unidirectionnal) and adds 1 tick delay
+// A gate that transmit the input signal to the output element (unidirectionnal) and adds 1 tick delay (1 update cycle to update)
 struct Diode {
 	inp u64 // id of the input element of the not gate
 	out u64 // id of the output of the not gate
@@ -54,11 +91,12 @@ struct Wire {
 
 struct App {
 	map []Chunk
+	actual_state int // indicate which list is the old state list and which is the actual one (0 for the first, 1 for the second)
 	nots []Nots
-	n_states []u8
+	n_states1 [2][]u8 // the old state and the actual state list
 	diodes []Diode
-	d_states []u8
+	d_states1 [2][]u8
 	ons []On
 	wires []Wire
-	w_states []u8
+	w_states1 [2][]u8 
 }
