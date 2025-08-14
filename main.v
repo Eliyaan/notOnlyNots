cec25ab1a2f9d087327a9aeb6ede41e20e701dff
Eliyaan <nopana1007@gmail.com>
2025-08-14 15:13:38 +0200

    fix leak and save crash

--- main.v
+++ main.v
@@ -2126,9 +2126,8 @@ fn (mut app App) computation_loop() {
 		}
 		now = time.now().unix_nano()
 		if app.todo.len == 0 && cycle_end - now >= 10000 { // 10micro sec
-			gc_disable()
+			gc_collect()
 			time.sleep((cycle_end - now) * time.nanosecond)
-			gc_enable()
 		}
 
 		now = time.now().unix_nano()
